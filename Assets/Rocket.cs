3182441378ca1b2ddeb11d9aa894054adcc78e23
Fabio Dantas <fabiolk2@hotmail.com>
2019-12-07 02:07:35 +0000

    Fix for movement bug and added obstacles

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,6 +7,9 @@ public class Rocket : MonoBehaviour {
     // Start is called before the first frame update
     Rigidbody rigidyBody;
     AudioSource audioSource;
+    [SerializeField] float rcsThrust = 300f;
+    [SerializeField] float mainThrust = 100f;
+
     void Start() {
         rigidyBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
@@ -14,21 +17,37 @@ public class Rocket : MonoBehaviour {
      
     // Update is called once per frame
     void Update() {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput() {
+    private void OnCollisionEnter(Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                break;
+            default:
+                print("Kill it fast dude!");
+                break;
+        }
+    }
+     
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
-            rigidyBody.AddRelativeForce(Vector3.up);
+            rigidyBody.AddRelativeForce(Vector3.up * mainThrust);
             PlayAudio();
         } else {
             StopAudio();
         }
+    }
+    private void Rotate() {
+        rigidyBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         } else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+        rigidyBody.freezeRotation = false;
     }
 
     private void PlayAudio() {
